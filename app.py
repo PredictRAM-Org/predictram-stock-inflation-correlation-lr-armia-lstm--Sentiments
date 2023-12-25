bd48951cc3466238369b41f7c603a211bfebae88
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-25 19:27:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ import streamlit as st
 from sklearn.linear_model import LinearRegression
 from pmdarima import auto_arima
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.layers import LSTM, Dense
 from sklearn.preprocessing import MinMaxScaler
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from newsapi import NewsApiClient
 from datetime import datetime, timedelta
 
 # Load CPI data
@@ -22,8 +21,8 @@ stock_files = [f for f in os.listdir(stock_folder) if f.endswith(".xlsx")]
 # Load stock news data
 stock_news_data = pd.read_excel("stock_news.xlsx")
 
-# Initialize NewsAPI client (please replace 'YOUR_API_KEY' with your actual NewsAPI key)
-newsapi = NewsApiClient(api_key='5843e8b1715a4c1fb6628befb47ca1e8')
+# Initialize NewsAPI client (replace 'YOUR_API_KEY' with your actual NewsAPI key)
+newsapi = NewsApiClient(api_key='YOUR_API_KEY')
 
 # Function to calculate correlation and build models
 def analyze_stock(stock_data, cpi_data, expected_inflation, selected_tenure_offset):
@@ -71,7 +70,11 @@ def analyze_stock(stock_data, cpi_data, expected_inflation, selected_tenure_offs
     model_arima = auto_arima(y_lr, seasonal=False, suppress_warnings=True)
 
     # Train LSTM model
-    scaled_data = min_max_scaler.fit_transform(y_lr.values.reshape(-1, 1))
+    scaled_data = y_lr.values.reshape(-1, 1)
+
+    if scaled_data.shape[1] != 1:
+        st.write(f"Error: Expected 1 column in scaled_data, but got {scaled_data.shape[1]} columns.")
+        return None
 
     x_train, y_train = prepare_data_for_lstm(scaled_data)
     model_lstm = build_lstm_model(x_train.shape[1])
@@ -91,7 +94,7 @@ def analyze_stock(stock_data, cpi_data, expected_inflation, selected_tenure_offs
 
     # Predict future prices using LSTM
     last_observed_price = scaled_data[-1]
-    future_price_lstm = predict_future_lstm(last_observed_price, model_lstm, min_max_scaler)
+    future_price_lstm = predict_future_lstm(last_observed_price, model_lstm)
     st.write(f"Predicted Stock Price for Future Inflation (LSTM): {future_price_lstm}")
 
     # Display the latest actual price
@@ -100,110 +103,31 @@ def analyze_stock(stock_data, cpi_data, expected_inflation, selected_tenure_offs
 
     return correlation_close_cpi, future_prices_lr[0], future_prices_arima, latest_actual_price, future_price_lstm, stock_name
 
-def prepare_data_for_lstm(data, look_back=1):
+def prepare_data_for_lstm(data, time_steps=1):
     x, y = [], []
-    for i in range(len(data) - look_back):
-        x.append(data[i:(i + look_back), 0])
-        y.append(data[i + look_back, 0])
+    for i in range(len(data) - time_steps):
+        x.append(data[i:(i + time_steps), 0])
+        y.append(data[i + time_steps, 0])
     return np.array(x), np.array(y)
 
 def build_lstm_model(input_shape):
     model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(input_shape, 1)))
-    model.add(LSTM(units=50))
+    model.add(LSTM(units=50, activation='relu', input_shape=(input_shape, 1)))
     model.add(Dense(units=1))
-    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.compile(optimizer='adam', loss='mse')
     return model
 
-def predict_future_lstm(last_observed_price, model, min_max_scaler, num_steps=1):
+def predict_future_lstm(last_observed_price, model):
+    num_steps = 1  # number of steps to forecast
     predicted_prices = []
-    input_data = last_observed_price.reshape(1, -1, 1)
 
     for _ in range(num_steps):
-        predicted_price = model.predict(input_data)
+        input_data = last_observed_price.reshape((1, len(last_observed_price), 1))
+        predicted_price = model.predict(input_data, verbose=0)
         predicted_prices.append(predicted_price[0, 0])
-        input_data = np.append(input_data[:, 1:, :], predicted_price.reshape(1, 1, 1), axis=1)
+        last_observed_price = np.append(last_observed_price[1:], predicted_price[0, 0])
 
-    return min_max_scaler.inverse_transform(np.array(predicted_prices).reshape(-1, 1))[-1, 0]
-
-# Function to perform sentiment analysis using VADER on stock_news_data
-def perform_sentiment_analysis(stock_name):
-    st.write(f"\nPerforming Sentiment Analysis for {stock_name}...")
-
-    # Check if the 'Stocks' column is present in stock_news_data
-    if 'Stocks' not in stock_news_data.columns:
-        st.write("Error: 'Stocks' column not found in stock_news_data. Please check your data.")
-        return []
-
-    # Extract news articles for the given stock_name
-    stock_news = stock_news_data[stock_news_data['Stocks'] == stock_name]
-
-    # Check if the 'News' column is present in stock_news_data
-    if 'News' not in stock_news.columns:
-        st.write("Error: 'News' column not found in stock_news_data. Please check your data.")
-        return []
-
-    news_titles = stock_news['News'].tolist()
-    sentiment_scores = []
-
-    # Initialize VADER SentimentIntensityAnalyzer
-    analyzer = SentimentIntensityAnalyzer()
-
-    for title in news_titles:
-        st.write(f"\nAnalyzing sentiment for a news article...")
-        try:
-            sentiment_scores.append(analyze_sentiment(analyzer, title))
-        except Exception as e:
-            st.write(f"Error analyzing sentiment for the article: {e}")
-            sentiment_scores.append(None)
-
-    return sentiment_scores
-
-def analyze_sentiment(analyzer, text):
-    compound_score = analyzer.polarity_scores(text)['compound']
-    return compound_score
-
-# Function to perform sentiment analysis using NewsAPI on stock_news_data
-def perform_sentiment_analysis_newsapi(stock_name):
-    st.write(f"\nPerforming Sentiment Analysis for {stock_name} using NewsAPI...")
-
-    # Check if the 'Stocks' column is present in stock_news_data
-    if 'Stocks' not in stock_news_data.columns:
-        st.write("Error: 'Stocks' column not found in stock_news_data. Please check your data.")
-        return []
-
-    # Extract news articles for the given stock_name
-    stock_news = stock_news_data[stock_news_data['Stocks'] == stock_name]
-
-    # Check if the 'News' column is present in stock_news_data
-    if 'News' not in stock_news.columns:
-        st.write("Error: 'News' column not found in stock_news_data. Please check your data.")
-        return []
-
-    news_titles = stock_news['News'].tolist()
-    sentiment_scores = []
-
-    for title in news_titles:
-        st.write(f"\nAnalyzing sentiment for a news article using NewsAPI...")
-        try:
-            sentiment_scores.append(analyze_sentiment_newsapi(title))
-        except Exception as e:
-            st.write(f"Error analyzing sentiment for the article using NewsAPI: {e}")
-            sentiment_scores.append(None)
-
-    return sentiment_scores
-
-def analyze_sentiment_newsapi(title):
-    # Make a call to the NewsAPI here
-    # Replace 'YOUR_API_KEY' with your actual NewsAPI key
-    response = newsapi.get_everything(q=title, language='en', sort_by='relevancy', page_size=1)
-
-    if response['status'] == 'ok' and response['totalResults'] > 0:
-        articles = response['articles']
-        compound_score = analyze_sentiment(analyzer, articles[0]['title'])
-        return compound_score
-    else:
-        return None
+    return predicted_prices[-1]
 
 # Streamlit UI
 st.title("Stock-CPI Correlation Analysis with Expected Inflation, Price Prediction, and Sentiment Analysis")
@@ -232,7 +156,6 @@ if train_model_button:
     future_prices_arima_list = []
     latest_actual_prices = []
     future_price_lstm_list = []
-    sentiment_scores_list = []
     stock_names = []
 
     for stock_file in stock_files:
@@ -241,18 +164,20 @@ if train_model_button:
         selected_stock_data.name = stock_file  # Assign a name to the stock_data for reference
 
         # Perform analysis based on user-selected tenure
-        correlation_close_cpi, future_price_lr, future_price_arima, latest_actual_price, future_price_lstm, stock_name = analyze_stock(selected_stock_data, cpi_data, expected_inflation, selected_tenure_offset)
+        analysis_result = analyze_stock(selected_stock_data, cpi_data, expected_inflation, selected_tenure_offset)
+        if analysis_result is not None:
+            correlation_close_cpi, future_price_lr, future_price_arima, latest_actual_price, future_price_lstm, stock_name = analysis_result
 
-        # Perform sentiment analysis using VADER
-        sentiment_scores = perform_sentiment_analysis(stock_name)
-        sentiment_scores_list.append(sentiment_scores)
+            # Perform sentiment analysis using VADER
+            sentiment_scores = perform_sentiment_analysis(stock_name)
+            sentiment_scores_list.append(sentiment_scores)
 
-        correlations.append(correlation_close_cpi)
-        future_prices_lr_list.append(future_price_lr)
-        future_prices_arima_list.append(future_price_arima)
-        latest_actual_prices.append(latest_actual_price)
-        future_price_lstm_list.append(future_price_lstm)
-        stock_names.append(stock_name)
+            correlations.append(correlation_close_cpi)
+            future_prices_lr_list.append(future_price_lr)
+            future_prices_arima_list.append(future_price_arima)
+            latest_actual_prices.append(latest_actual_price)
+            future_price_lstm_list.append(future_price_lstm)
+            stock_names.append(stock_name)
 
     # Display overall summary in a table
     summary_data = {
@@ -266,12 +191,3 @@ if train_model_button:
     summary_df = pd.DataFrame(summary_data)
     st.write("\nCorrelation and Price Prediction Summary:")
     st.table(summary_df)
-
-    # Display sentiment scores using VADER in a table
-    sentiment_data = {
-        'Stock': stock_names,
-        'Sentiment Scores (VADER)': sentiment_scores_list
-    }
-    sentiment_df = pd.DataFrame(sentiment_data)
-    st.write("\nSentiment Analysis Summary (VADER):")
-    st.table(sentiment_df)
